4459e6c4416b13c3ba83ff725d39900bead12d11
Gaurang Agarwal <gaurang.agarwal1404@gmail.com>
2017-06-11 21:10:43 -0700

    Main Hompegae

---
+++ index.php
@@ -0,0 +1,168 @@
+<?php
+//echo system
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>HPCL</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<header>
+<div id="logo">
+	<center><img src="hpcl_logo.png" alt="Welcome to HPCL"></center>
+	<div id="script" style="float: right; margin-right: 10px; margin-top: -4px; padding-left: 2px; background-color: #000066;  font-size: 10px; color: white;">
+	<script type="text/javascript">
+			document.write ('<p>Current date & time:<br> <span id="date-time">', new Date().toLocaleString(), '<\/span>.<\/p>')
+			if (document.getElementById) onload = function () {
+				setInterval ("document.getElementById ('date-time').firstChild.data = new Date().toLocaleString()", 50)
+			}
+	</script>
+	</div>
+</div>
+<div id="nav_bg">
+<ul>
+	<li><a href="index.php">HOME</a></li>
+	<li><a href="organisation.php">ORGANISATION</a></li>
+	<li><a href="contact.php">CONTACT US</a></li>
+	<li id="sec"><a href="login.php">ADMIN LOGIN</a></li>
+</ul>
+</div>
+</header>
+<main>
+<!--<div><img src="bnimg.png" width="100%" style="padding-top: 20px;"></div>-->
+<br>
+<div style="width:100%;border:0px solid #000;">
+    <div style="float:left; width:29%; background:#fff; height:100%;">
+    <fieldset style="height: 20%; margin-top: 10px;"">
+        <div style="width:100%; height : 50%; border:0px solid #000;">
+        <div style="width: 100%; height: 10%; background-color: #002266; text-align: center;"><font color="white">HPCL FEATURES</font></div>
+        <table>
+    	
+    	<tr><td>New Delhi</td><td>:Laxmi Nagar</td></tr>
+    	<tr><td>Lucknow</td><td>:Hazrat Ganj</td></tr>
+    	<tr><td>Kanpur</td><td>:Swaroop Nagar</td></tr>
+    	<tr><td>Agra</td><td>:Pithrauli</td></tr>
+    	<tr><td>Varanasi</td><td>:12th Street</td></tr>
+    	<tr><td>Dadri</td><td>:MGR Road</td></tr>
+    	<tr><td>Revadi</td><td>:Khilana District</td></tr>
+    </table>
+    </fieldset>
+    <fieldset>
+        <div style="width:100%; height : 100%; border:0px solid #000; margin-top: 10px;">
+        <div style="width: 100%; height: 10%; background-color: #002266; text-align: center;"><font color="white">HPCL NEWS</font>
+    </div>
+        <div style="width: 100%; height: 20%; background-color: #f1f1f1; text-align: center; margin-right:5px; margin-top: 10px;padding-bottom: 10px;">
+            <marquee direction="up">
+               <p><img src="news.png" align="left" height="10px" width="10px"><b><a href="http://www.moneycontrol.com/news/business/hpcl-gets-green-nod-for-rs-230-cr-rbpl-expansion-project-2301001.html">HPCL gets green nod for Rs 230-cr RBPL expansion project</a></b><br>Hindustan Petroleum Corp Ltd (HPCL) operates a 243 km long cross-country pipeline RBPL for transportation of various petroleum products.</p>
+               <!--second news -->
+               <p><img src="news.png" align="left" height="10px" width="10px"><b><a href="http://www.moneycontrol.com/news/business/companies/hpcl-mrpl-merger-brings-a-lot-of-synergies-makes-lot-of-sense-hpcl-2300889.html">
+            HPCL-MRPL merger brings a lot of synergies and makes lot of sense: HPCL</a></b><br>In an interview to CNBC-TV18, MK Surana, CMD of HPCL spoke about the latest happenings in his company and sector. “Merger of HPCL and MRPL brings a ...</p>
+            <br>
+            </marquee>
+        </div>
+    </fieldset>
+    </div>
+     <!-- first gap div -->
+    <div style="float:left; width:0.5%; background:#fff; height:100%;"><p width="0.5%" height="100px"></p></div>
+    <!-- second div -->
+    <center><div style="float:left; width:29%; background:#f1f1f1; height:100%; padding-top: 10px;padding-bottom: 10px;">
+    <fieldset>
+    	<div style="width:100%;border:0px solid #000;">
+    	<div style="float:left; width:30%; background:#f1f1f1; height:100%;"><img src="tele1.png" height="50px" width="50px"></div>
+    <div style="float:left; width:10%; background:#f1f1f1; height:100%;"><p width="0.5%" height="100px"></p></div>
+    <div style="float:left; width:70%; background:#f1f1f1; height:100%;"><b>Can We help you for Buisness related issues ?</b></div>
+    <div style="clear:both;"></div>
+    <br>
+    <table>
+    	<tr>
+    		<td><b>Central Control</b></td>
+    		<td>:011-23345687</td>
+    	</tr>
+    	<tr>
+    		<td><b>New Delhi</b></td>
+    		<td>:011-23345686</td>
+    	</tr><tr>
+    		<td><b>Mumbai</b></td>
+    		<td>:011-23345685</td>
+    	</tr>
+    	<tr>
+    		<td><b>Kanpur</b></td>
+    		<td>:011-23345684</td>
+    	</tr>
+    	<tr>
+    		<td><b>Revadi</b></td>
+    		<td>:011-23345683</td>
+    	</tr>
+    	<tr>
+    		<td><b>Varanasi</b></td>
+    		<td>:011-23345682</td>
+    	</tr>
+    </table>
+    <hr>
+    <table>
+    	<tr>
+    		<td><b>Toll Free</b></td>
+    		<td>:1800200758</td>
+    	</tr>
+    	<tr>
+    		<td><b>SMS</b></td>
+    		<td>:9765432854</td>
+    	</tr><tr>
+    		<td><b>Write to</b></td>
+    		<td>:feedback@hpcl.com</td>
+    	</tr>
+    	<tr>
+    		<td><b>For Network Connection Complaints</b></td>
+    		<td>:care@hpcl.com</td>
+    	</tr>
+    	<tr>
+    		<td><b>For Further Queries</b></td>
+    		<td>:info@hpcl.com</td>
+    	</tr>
+    </table>
+    <hr>
+    <table>
+    	<center><tr bgcolor="#002266" style="color: white;">
+    		<th><b>CENTERS IN INDIA</b></th>
+    	</tr></center>
+    	<tr><td>New Delhi</td><td>:Laxmi Nagar</td></tr>
+    	<tr><td>Lucknow</td><td>:Hazrat Ganj</td></tr>
+    	<tr><td>Kanpur</td><td>:Swaroop Nagar</td></tr>
+    	<tr><td>Agra</td><td>:Pithrauli</td></tr>
+    	<tr><td>Varanasi</td><td>:12th Street</td></tr>
+    	<tr><td>Dadri</td><td>:MGR Road</td></tr>
+    	<tr><td>Revadi</td><td>:Khilana District</td></tr>
+    </table>
+    </fieldset>
+    </div>
+    <div style="float:left; width:0.5%; background:#fff; height:100%;"><p width="0.5%" height="100px"></p></div>
+    <div style="float:left; width:30%; background:#fff; height:100%; padding-top: 10px;">
+    		<fieldset>
+    	<div style="height:100%;border:0px solid #000;">
+    	<center><div style="height:30%; background:#fff; width:100%;"><img src="hpcl.png"></div></center>
+    <div style="height:20%; background:#fff; width:100%;"></div>
+    <center><div style="height:50%; background:#fff; width:100%; margin-top: 5px; padding-top: 5px;"><b>To Visit the official website of HPCL Click the below link !</b><br><a href="http://www.hindustanpetroleum.com">Hindustan Petroleum Corporation Limited</a></div></center>
+    <div style="clear:both;"></div>
+    <hr>
+    <!--login div style> -->
+    	<p><img src="lgme.png" align="left" height="80px" width="300px"><div style="height:100%;border:0px solid #fff;">
+    		<div style=" float: right; height:40%; background:#fff; width:30%; margin-top: 15px;"><a href="emplgn.php"><button style="width: 100%; background-color: #66ff33; height: 50px; color: white;"><p><img src="emplogo.png" align="left" height="20px" width="20px"><b>Employee Login<b></b></button></a></div>
+    		<div style="height:20%; background:#fff; width:100%;margin-top: 4px;"></div>
+    		<div style="clear:both;"></div></p>
+    <hr>
+    <div style="width: 100%; height: 100%; border:0px solid #000;">
+    <div style="float: right; width: 100%;height: 50%;">
+        <img src="img2.png" width="450px" height="275px">   
+    </div>
+    <div style="clear:both;"></div>
+    </fieldset>	
+    </div>
+    <div style="clear:both;"></div>
+</div>
+</main>
+<footer>
+	Copyrights reserved by HPCL 
+</footer>
+</body>
+</html>
